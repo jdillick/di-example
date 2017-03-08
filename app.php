388d88eb8b9309b0ee8f3302952ed218ebc6d296
John Dillick <jdillick@resourceammirati.com>
2017-03-07 23:07:30 -0500

    Add dependency injection container.

--- app.php
+++ app.php
@@ -6,6 +6,7 @@ use \App\Core;
 use HttpKernel\Router;
 use HttpKernel\RouteFactory;
 use HttpKernel\Request;
+use DI\ContainerBuilder;
 
-$core = new Core(new Router, new RouteFactory);
-$core->serve(new Request($_SERVER));
+$container = ContainerBuilder::buildDevContainer();
+$container->get('App\Core')->serve(new Request($_SERVER));
